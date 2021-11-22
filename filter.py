2cf0136928740aaf51b271544970572c90369695
dashanoskova <drdahey18@gmail.com>
2021-11-22 17:23:51 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -1,28 +1,39 @@
 from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n0 in range(j, j + 10):
-                n1 = int(arr[n][n0][0])
-                n2 = int(arr[n][n0][1])
-                n3 = int(arr[n][n0][2])
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50 / 3
-                arr[n][n1][1] = int(s // 50) * 50 / 3
-                arr[n][n1][2] = int(s // 50) * 50 / 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+pixels = np.array(img)
+width = len(pixels)
+height = len(pixels[1])
+
+
+def get_brightness(pixels, mosaic_size, i, j):
+    brightness = 0
+    for line in range(i, i + mosaic_size):
+        for column in range(j, j + mosaic_size):
+            red = int(pixels[line][column][0])
+            green = int(pixels[line][column][1])
+            blue = int(pixels[line][column][2])
+            color = red + blue + green
+            brightness += color
+    return int(brightness // mosaic_size ** 2)
+
+
+def change_pixels(pixels, mosaic_size, brightness, step, i, j):
+    for line in range(i, i + mosaic_size):
+        for column in range(j, j + mosaic_size):
+            pixels[line][column][0] = int(brightness // step) * step / 3
+            pixels[line][column][1] = int(brightness // step) * step / 3
+            pixels[line][column][2] = int(brightness // step) * step / 3
+    return pixels
+
+
+def get_grey(pixels, mosaic_size, step):
+    for i in range(0, height - mosaic_size + 1, mosaic_size):
+        for j in range(0, width - mosaic_size + 1, mosaic_size):
+            brightness = get_brightness(pixels, mosaic_size, i, j)
+            pixels = change_pixels(pixels, mosaic_size, brightness, step, i, j)
+    return pixels
+
+
+res = Image.fromarray(get_grey(pixels, 10, 50))
 res.save('res.jpg')
